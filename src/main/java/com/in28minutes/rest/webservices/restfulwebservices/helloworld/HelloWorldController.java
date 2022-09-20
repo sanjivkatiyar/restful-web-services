17d67b64e575a4f63d11cb5a4d4193adcb317680
sanjiv <sanjiv.katiyar@gmail.com>
2022-09-20 12:30:17 -0700

    ok

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,13 @@ public class HelloWorldController {
         return "hello-world";
     }
 
-    @RequestMapping(path = "/hello-Sanjiv", method = RequestMethod.GET)
+    @GetMapping(path = "/hello-Sanjiv")
     public String helloSanjiv(){
         return "hello-Sanjiv";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("hello-world");
     }
 }
